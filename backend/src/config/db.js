1de572b3f76dc0d1ae79a455634cd1202b7788ad
byashpaswan <byas.paswan99@gmail.com>
2026-05-25 12:48:21 +0530

    add debug

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,6 +2,8 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
+    console.log("Connecting to MongoDB...");
+    console.log("MONGO_URI:", process.env.MONGO_URI); // Debugging line
     if (!process.env.MONGO_URI) {
       throw new Error("MONGO_URI is not configured");
     }
